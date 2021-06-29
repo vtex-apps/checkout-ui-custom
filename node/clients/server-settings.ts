c9af700adf621cf112518098d9162450824cf780
Wender Lima <wender.lima@vtex.com.br>
2021-06-29 19:59:34 -0300

    CHECKOUTUI-3 Now compiled files will be pushed to VBase to improve performance

---
+++ node/clients/server-settings.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { InstanceOptions, IOContext, AppClient } from '@vtex/api'
+
+export class SaveVB extends AppClient {
+  constructor(ctx: IOContext, options?: InstanceOptions) {
+    super(`vtex.checkout-ui-settings-server@0.x`, ctx, options)
+  }
+
+  public saveVB = async (data: any) => {
+    await this.http
+      .post('/_v/checkout-ui-custom/savevb', data, {
+        headers: { 'content-type': 'application/json' },
+      })
+      .catch(err => {
+        throw new Error(err)
+      })
+  }
+}
