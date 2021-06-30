1ca5bed6bb90de367763ee068af92140f4265620
Wender Lima <wender.lima@vtex.com.br>
2021-06-30 16:16:50 -0300

    Increasing timeout

--- node/index.ts
+++ node/index.ts
@@ -4,7 +4,7 @@ import { Service, ServiceContext, ClientsConfig, LRUCache } from '@vtex/api'
 import { Clients } from './clients'
 import { resolvers } from './graphql'
 
-const TIMEOUT_MS = 2000
+const TIMEOUT_MS = 4000
 const defaultClientOptions = {
   retries: 1,
   timeout: TIMEOUT_MS,
