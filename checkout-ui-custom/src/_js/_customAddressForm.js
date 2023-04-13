876498d87b0dc98338f9c74f0370a078d1c5182f
Lucas Vyskubenko <lucas.vysk@vtex.com.br>
2023-04-13 18:30:19 -0300

    fix lint 4

--- checkout-ui-custom/src/_js/_customAddressForm.js
+++ checkout-ui-custom/src/_js/_customAddressForm.js
@@ -93,9 +93,7 @@ class fnsCustomAddressForm {
     geoCoordinates = ''
   ) {
     $('.vcustom--vtex-omnishipping-1-x-address #v-custom-ship-street').val(
-      'number' in this.addressrules
-      ? street
-      : formattedStreet || street
+      'number' in this.addressrules ? street : formattedStreet || street
     )
     $('.vcustom--vtex-omnishipping-1-x-address #ship-complement').val(
       complement
@@ -107,9 +105,7 @@ class fnsCustomAddressForm {
     )
     $('.vcustom--vtex-omnishipping-1-x-address #v-custom-ship-street').attr(
       'data-street',
-      'number' in this.addressrules
-      ? street
-      : formattedStreet || street
+      'number' in this.addressrules ? street : formattedStreet || street
     )
     $('.vcustom--vtex-omnishipping-1-x-address #v-custom-ship-street').attr(
       'data-number',
