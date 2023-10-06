b795da7295dfaecde11deb1d01847a645032deba
beatrizmaselli <beatriz.maselli@vtex.com.br>
2023-10-06 12:08:04 +0200

    add validation

--- checkout-ui-custom/src/_js/_v.custom.checkout.ui.js
+++ checkout-ui-custom/src/_js/_v.custom.checkout.ui.js
@@ -284,10 +284,16 @@ class checkoutCustom {
     0)
 
     // Match coupon with rateAndBenefitsIdentifiers
+    let couponMatch = null
 
-    const couponMatch = orderForm.ratesAndBenefitsData.ratesAndBenefitsIdentifiers.find(
-      item => item.name === _coupon
-    )
+    if (
+      orderForm.ratesAndBenefitsData &&
+      orderForm.ratesAndBenefitsData.rateAndBenefitsIdentifiers.length
+    ) {
+      couponMatch = orderForm.ratesAndBenefitsData.ratesAndBenefitsIdentifiers.find(
+        item => item.name === _coupon
+      )
+    }
 
     if (!_coupon || couponItemsCount > 0 || couponMatch) {
       $('fieldset.coupon-fieldset').removeClass(
