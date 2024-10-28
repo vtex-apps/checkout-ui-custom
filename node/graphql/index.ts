a2206072977019a6972bc15ad258f15a7758395c
Lucas Vyskubenko <lucas.vysk@vtex.com.br>
2024-10-28 11:26:47 -0300

    feat: validate app version

--- node/graphql/index.ts
+++ node/graphql/index.ts
@@ -111,16 +111,25 @@ export const resolvers = {
       } = ctx
 
 
-      
+      const app: string = getAppId()
+
+      const version = app.split(`@`)[1];
 
       const cookie = ctx.headers?.cookie;
-
 
       const vtexCredentials:any = cookie ? 
             cookie.split('; ')
             .find((cookie: string) => cookie.startsWith('VtexIdclientAutCookie='))
             ?.split('=')[1] ?? ''
         : '';
+
+      if(version <= `0.18.9`) {
+
+        logger.warn({
+          message: 'Error: Invalid version'
+        })
+        throw new ForbiddenError('Unauthorized version')
+      }
 
       if(!vtexCredentials) {
 
